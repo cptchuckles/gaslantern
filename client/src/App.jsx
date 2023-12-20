ecd9c3f22add8030f89ae1dcb9503a36e3b61f3b
Grendel <jcg@grendel.world>
2023-12-20 12:51:52 -0600

    stub client/ and server/ projects

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>GasLantern®</h1>
+      <p>Your source of truth when you're confused™</p>
+    </>
+  )
+}
+
+export default App
